7f6f5fd653d45fcf69b417fc1bbb385c7827a66d
duncan <duncan.kampert@surf.nl>
2024-07-19 14:54:25 +0200

    Tried stuff but its a bit slow

--- dcgmi_top.py
+++ dcgmi_top.py
@@ -1,15 +1,17 @@
 import argparse
+import curses
 import io
-import os
-import time
+import locale
+import subprocess
+import threading
 from collections import defaultdict
 from contextlib import redirect_stdout
+from functools import partial
 
 import plotext as plt
-import subprocess
-import curses
-import sys
 
+locale.setlocale(locale.LC_ALL, '')
+code = locale.getpreferredencoding()
 COLUMNS = ["GPUTL", "MCUTL", "GRACT", "SMACT", "SMOCC", "TENSO", "DRAMA", "PCITX", "PCIRX", "NVLTX",
            "NVLRX", "POWER"]
 SCALES = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -20,7 +22,7 @@ class Plotter():
     def __init__(self, gpus, metrics, refresh_rate, **kwargs):
         self.popen = None
         self.gpu_nodes = gpus
-        self.metrics = metrics.split(",")
+        self.metrics: list[str] = metrics.split(",")
 
         self.refresh_rate = int(refresh_rate)
         assert len(self.gpu_nodes.split(",")) <= 4
@@ -29,6 +31,11 @@ class Plotter():
 
         self.cmd = f"dcgmi dmon -e 203,204,1001,1002,1003,1004,1005,1009,1010,1011,1012,155 -i {self.gpu_nodes} -d {self.refresh_rate}".split()
         self.per_entity_data = defaultdict(list)
+        self.metric_selected_id = 0
+
+        self._running = True
+        self.key_thread = None
+        self.data_thread = None
 
     def get_power_limit(self):
         response = subprocess.run("nvidia-smi -q -d POWER".split(" "), stdout=subprocess.PIPE).stdout.decode("utf-8")
@@ -62,62 +69,97 @@ class Plotter():
         metric_id = COLUMNS.index(metric_name)
         scale = SCALES[metric_id]
         metrics = self.per_entity_data[gpu_name]
-        if len(self.per_entity_data[gpu_name]) > 30:
-            self.per_entity_data[gpu_name] = metrics[-30:]
         current_metrics = [self.tryfloat(x[metric_id]) / scale for x in self.per_entity_data[gpu_name]]
         return [0.0] * (30 - len(current_metrics)) + current_metrics
 
+    def print_menu(self, stdscr):
+        start_color = '\033[43;30m'
+        end_color = '\033[0m'
+
+        metric_list_str = ""
+        for i, metric in enumerate(COLUMNS):
+            metric_list_str += " "
+            if self.metric_selected_id == i:
+                metric_list_str += start_color
+            is_active = "O" if metric in self.metrics else "X"
+            metric_list_str += f"[{metric}|{is_active}]"
+            if self.metric_selected_id == i:
+                metric_list_str += end_color
+
+        print(f"Metrics{metric_list_str}")
+
+    def handle_inputs(self, stdscr):
+        while self._running:
+            key = stdscr.getch()
+            if key == ord("q"):
+                self._running = False
+                break
+            if key == 260:  # Left arrow
+                self.metric_selected_id = max(0, self.metric_selected_id - 1)
+            if key == 261:  # right arrow
+                self.metric_selected_id = min(len(COLUMNS) - 1, self.metric_selected_id + 1)
+            if key == 10:
+                metric = COLUMNS[self.metric_selected_id]
+                if metric in self.metrics:
+                    self.metrics.remove(metric)
+                else:
+                    self.metrics.append(metric)
+
+    def collect_data(self):
+        for data_point in self.execute():
+            if not self._running:
+                break
+
+            if data_point.startswith("#Entity") or data_point.startswith("ID"):
+                continue
+            elems = [x.strip() for x in data_point.split("  ") if x.strip() != ""]
+            self.per_entity_data[elems[0]].append(elems[1:])
+
+            for gpu_name in self.per_entity_data.keys():
+                if len(self.per_entity_data[gpu_name]) > 30:
+                    self.per_entity_data[gpu_name] = self.per_entity_data[gpu_name][-30:]
+
     def run(self, stdscr):
         curses.curs_set(0)  # Hide the cursor
+        stdscr.nodelay(True)  # Make getch non-blocking
         stdscr.clear()  # Clear the screen
         stdscr.encoding = 'utf-8'
 
-        xs = list(range(30))
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)
 
-        if stdscr is not None:
-            curses.start_color()
-            curses.use_default_colors()
+        self.key_thread = threading.Thread(target=partial(self.handle_inputs, stdscr))
+        self.key_thread.start()
 
-        iter_id = 0
-        for data_point in self.execute():
-            if data_point.startswith("#Entity") or data_point.startswith("ID"):
-                continue
-            iter_id += 1
-            elems = [x.strip() for x in data_point.split("  ") if x.strip() != ""]
-            self.per_entity_data[elems[0]].append(elems[1:])
+        self.data_thread = threading.Thread(target=self.collect_data)
+        self.data_thread.start()
+
+        while self._running:
             gpus = self.gpu_nodes.split(",")
-
-            if iter_id % len(gpus) != 0:
-                continue
 
             plot_lines = self.get_plot_lines(gpus, stdscr)
             plot_lines.seek(0)
 
             stdscr.clear()
             stdscr.refresh()
+            maxy, maxx = stdscr.getmaxyx()
 
             lines = plot_lines.readlines()
             for y, line in enumerate(lines):
-                print(line[:-1], end="")
-                # stdscr.addstr(y, 0, line[:-1])
-                # # Ensure we do not write outside the window height
-                # if y >= max_y:
+                # if y >= maxy:
                 #     break
-                # # Truncate the line to fit within the window width
-                # line = line[:max_x]
-                # try:
-                #     print(line[:-1], end="")
-                # except curses.error:
-                #     pass  # Handle any curses errors gracefully
-            print(len(lines), stdscr.getmaxyx())
-            # stdscr.refresh()
-            # stdscr.getch()
+                # for x, ch in enumerate(line[:-1]):
+                #     if x >= maxx:
+                #         break
+                #     print(ch, end="")
+                #     stdscr.addstr(y, x, ch.encode(code))
+                print(line[:-1], end="")
 
-            if stdscr is not None:
-                stdscr.refresh()
-                stdscr.timeout(self.refresh_rate)
-            else:
-                time.sleep(self.refresh_rate / 1000)
+            self.print_menu(stdscr)
+
+            stdscr.refresh()
+            curses.napms(1000)
 
     def get_plot_lines(self, gpus, stdscr):
         width = 2 if len(gpus) > 1 else 1
@@ -141,6 +183,14 @@ class Plotter():
             plt.show()
         return plot_chars
 
+    def kill(self):
+        self._running = False
+        if self.key_thread is not None:
+            self.key_thread.join()
+        if self.data_thread is not None:
+            self.data_thread.join()
+        self.popen.kill()
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -155,14 +205,12 @@ def main():
     plotter = Plotter(**vars(args))
 
     try:
-        # plotter.run(None)
-        # TODO: Fix this
         screen = curses.wrapper(plotter.run)
     except KeyboardInterrupt as e:
         pass
     except Exception as e:
         print(e)
-    plotter.popen.kill()
+    plotter.kill()
 
 
 if __name__ == "__main__":
